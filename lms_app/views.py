978caa58896fbf6d3e74e6ad04250a728cc2b544
IvyMbugua <58328949+IvyMbugua@users.noreply.github.com>
2024-02-21 21:20:42 +0300

    fixed error on homepage

--- lms_app/views.py
+++ lms_app/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Assignment, Question
 
 def home(request):
-    return render(request, 'lms_app/home.html')
+    return render(request, 'home.html')
 
 def assignment_list(request):
     assignments = Assignment.objects.all()
-    return render(request, 'lms_app/assignment_list.html', {'assignments': assignments})
+    return render(request, 'assignment_list.html', {'assignments': assignments})
 
 def assignment_detail(request, assignment_id):
     assignment = get_object_or_404(Assignment, pk=assignment_id)
-    return render(request, 'lms_app/assignment_detail.html', {'assignment': assignment})
+    return render(request, 'assignment_detail.html', {'assignment': assignment})
